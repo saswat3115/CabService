6987acc4edbb908b79000498fee0f827f65e9236
Saswat Mohanty <saswat.mohanty@in.tesco.com>
2017-09-09 02:56:06 +0530

    search box added to menu

--- app/fileUpload.js
+++ app/fileUpload.js
@@ -1,13 +1,13 @@
 var multer = require('multer');
+var cookieParser = require('./cookieParser');
 
 
 var storage = multer.diskStorage({ //multers disk storage settings
     destination: function (req, file, cb) {
         cb(null, 'storage/uploads/');
     },
-    filename: function (req, file, cb) {
-        var datetimestamp = Date.now();
-        cb(null, file.fieldname + '-' + datetimestamp + '.' + file.originalname.split('.')[file.originalname.split('.').length -1]);
+    filename: function (req, file, cb) {        
+        cb(null, file.fieldname + '-' + Date.now() + '.' + file.originalname.split('.')[file.originalname.split('.').length -1]);
     }
 });
 
