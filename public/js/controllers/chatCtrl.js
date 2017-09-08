0bdb93695a9441046ef6b2ae0dd15e13187c4952
Saswat Mohanty <saswat.mohanty@in.tesco.com>
2017-09-09 03:08:18 +0530

    removed unessary logs

--- public/js/controllers/chatCtrl.js
+++ public/js/controllers/chatCtrl.js
@@ -1,13 +1,11 @@
 angular.module('ChatCtrl',[]).controller('ChatController', function($scope, $location, apiService, sessionService){        
 
-    $scope.username = sessionService.getCookie('loggedinUser').fname;   
-    console.log(sessionService.getCookie('loggedinUser').fname);
+    $scope.username = sessionService.getCookie('loggedinUser').fname;       
 
     $scope.allusers = [];
 
     $scope.getAll = function(){
-        apiService.getAllUser().then(function(result){
-            console.log(JSON.stringify(result));
+        apiService.getAllUser().then(function(result){            
             $scope.allusers = result.data;
         });
     }
